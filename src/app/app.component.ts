103495c9a9bf364a2839bc28c4501737e8ede6da
lyra-school <s00233718@atu.ie>
2024-02-06 13:15:09 +0000

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'movie-list';
+
+  movies:any[]=[];
+  
+  constructor(private movieService:MovieService) {}
+
+  ngOnInit() {
+    this.movies=this.movieService.getMovies();
+    console.log(this.movies);
+  }
 }
